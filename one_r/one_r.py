96abc93edcf2016e37a08b78b792755117315abd
Ivan Rubinic <ivan.rubinic@outlook.com>
2023-08-03 11:59:20 +0200

    OneR multiclass, uneven quantity of predictions, unable to show complex interactions

--- one_r/one_r.py
+++ one_r/one_r.py
@@ -45,18 +45,43 @@ def one_r(X, y):
     return best_predictor
 
 def one_r_multiclass(X, y):
-    unique_classes = y.unique()
-    all_rules = {}
+    # Array of tuples - (class, number of instances of that class)
+    classes_counts = [(c, sum(y == c)) for c in y.unique()]
+    classes_counts.sort(key=lambda x: x[1])
+
+    rules = []
+
+    print(classes_counts, y.value_counts(ascending=True))
+
+    # While there are classes for which the rule hasn't been set yet
+    while len(classes_counts) > 1:
+        # Current class for which a rule should be set
+        c = classes_counts[0][0]
+
+        # Copy of the dataset to be modified to binary classification
+        yc = y.copy()
+        yc.loc[yc == c] = 1
+        yc.loc[yc != c] = 0
+
+        # Calculating the rule for the current class
+        rule = one_r(X, yc)
+        rules.append({'class': c, 'rule': rule})
+
+        # Removing the class for which the rule has been calculated
+        classes_counts.remove(classes_counts[0])
+
+    print(rules)
+
     
     
 
 data = pd.read_csv('../encoded_categorical.csv', index_col=False)
 X, y = data.iloc[:, :-1], data.iloc[:, -1]
 
-best_predictor = one_r(X, y)
-print("Best predictor: ", best_predictor)
+# best_predictor = one_r(X, y)
+# print("Best predictor: ", best_predictor)
 
-# rules = one_r_multiclass(X, y)
-# print("Learned Rules:")
+rules = one_r_multiclass(X, y)
+print("Learned Rules:")
 # for target_class, rule in rules.items():
 #     print(f"For class '{target_class}': {rule}")
