30b8a0e6b84bdea3f92ca43b8e19b527bfe9a05c
Ivan Rubinic <ivan.rubinic@outlook.com>
2023-08-22 13:22:31 +0200

    Sequential covering v2, code formatted

--- seqcov/learn_one_rule.py
+++ seqcov/learn_one_rule.py
@@ -3,6 +3,7 @@ from sklearn import tree
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class LearnOneRule:
     def __init__(self, data, max_depth, min_samples_leaf):
         self.data = data
@@ -12,33 +13,44 @@ class LearnOneRule:
         X, y = self.data.iloc[:, :-1], pd.DataFrame(self.data.iloc[:, -1])
         self.X_encoded = pd.get_dummies(X)
 
-        self.feature_names, self.class_names = self.X_encoded.columns, self.data['Preoperative Diagnosis'].unique().astype(str)
+        self.feature_names, self.class_names = self.X_encoded.columns, self.data['Preoperative Diagnosis'].unique(
+        ).astype(str)
 
-        self.clf = tree.DecisionTreeClassifier(max_depth=self.max_depth, min_samples_leaf=self.min_samples_leaf)
+        self.clf = tree.DecisionTreeClassifier(
+            max_depth=self.max_depth, min_samples_leaf=self.min_samples_leaf)
         self.clf = self.clf.fit(self.X_encoded.to_numpy(), y.to_numpy())
 
     def plot_classifier(self):
         plt.figure(figsize=(19, 11))
-        tree.plot_tree(self.clf, filled=True, feature_names=self.feature_names, class_names=self.class_names)
+        tree.plot_tree(self.clf, filled=True,
+                       feature_names=self.feature_names, class_names=self.class_names)
 
     # Finds the decision tree's node with the minimum gini value
     # Returns the node's gini value, the node's decision path (list),
     # the predicted class and the number of covered samples
-    def learn_one_rule(self, id, cond_feature, cond_is_right, rules):
+    def learn_one_rule(self, id, cond_feature, cond_operator, cond_threshold, rules):
         id_left, id_right = self.clf.tree_.children_left[id], self.clf.tree_.children_right[id]
-        feature = self.clf.tree_.feature[id]
+        current_feature = self.clf.tree_.feature[id]
+        current_threshold = self.clf.tree_.threshold[id]
 
         if cond_feature is not None:
-            rules.append((cond_feature, cond_is_right))
+            rules.append({
+                'feature': cond_feature,
+                'operator': cond_operator,
+                'threshold': cond_threshold
+            })
 
         # Leaf node
         if id_left == -1 and id_right == -1:
-            # print(id, self.clf.tree_.impurity[id], rules, self.clf.tree_.value[id], np.argmax(self.clf.tree_.value[id]), self.clf.tree_.n_node_samples[id])
+            # print(id, self.clf.tree_.impurity[id], rules, self.clf.tree_.value[id], np.argmax(
+            #     self.clf.tree_.value[id]), self.clf.tree_.n_node_samples[id])
             return [self.clf.tree_.impurity[id], rules, np.argmax(self.clf.tree_.value[id]), self.clf.tree_.n_node_samples[id]]
 
         # Propagate to left and right nodes
-        mgn_left, rules_left, class_left, covered_left = self.learn_one_rule(self.clf.tree_.children_left[id], self.feature_names[feature], 0, rules.copy())
-        mgn_right, rules_right, class_right, covered_right = self.learn_one_rule(self.clf.tree_.children_right[id], self.feature_names[feature], 1, rules.copy())
+        mgn_left, rules_left, class_left, covered_left = self.learn_one_rule(
+            self.clf.tree_.children_left[id], self.feature_names[current_feature], '<=', current_threshold, rules.copy())
+        mgn_right, rules_right, class_right, covered_right = self.learn_one_rule(
+            self.clf.tree_.children_right[id], self.feature_names[current_feature], '>', current_threshold, rules.copy())
 
         if mgn_left < mgn_right or (mgn_left == mgn_right and covered_left >= covered_right):
             return [mgn_left, rules_left, class_left, covered_left]
--- seqcov/main.py
+++ seqcov/main.py
@@ -3,11 +3,14 @@ from sklearn import tree
 from learn_one_rule import LearnOneRule
 import matplotlib.pyplot as plt
 
+
 def learn_one_rule(X, y):
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(X, y)
 
 # rule - list of (column, value) tuples used to filter the data
+
+
 def remove_covered_instances(data, rule):
     data.reset_index(drop=True, inplace=True)
 
@@ -19,24 +22,34 @@ def remove_covered_instances(data, rule):
     # Creating condition for removing instances
     # (all instances under remove condition should be removed)
     remove_condition = pd.Series([True] * len(data))
-    for col_name, value in rule:
-        remove_condition &= (data_encoded[col_name] == value)
-    
+    for condition in rule:
+        feat, op, thr = condition['feature'], condition['operator'], condition['threshold']
+
+        if op == '<=':
+            remove_condition &= (data_encoded[feat] <= thr)
+        elif op == '>':
+            remove_condition &= (data_encoded[feat] > thr)
+        else:
+            print("main.py::remove_covered_instances WARNING: Unknown operator!")
+
     # Filtering instances which should be removed (because they're covered by the new_rule)
     new_data = data[~remove_condition]
 
     return new_data
 
+
 def sc(data):
     rules_preds = []
-    
+
     while len(data) > 0:
         data.reset_index(drop=True, inplace=True)
-        
-        lor = LearnOneRule(data.copy(), max_depth=5, min_samples_leaf=1)
-        _, new_rule, pred, n_covered = lor.learn_one_rule(0, None, None, [])
-        plt.show()
-        
+
+        lor = LearnOneRule(data.copy(), max_depth=3, min_samples_leaf=1)
+        _, new_rule, pred, n_covered = lor.learn_one_rule(
+            0, None, None, None, [])
+        # lor.plot_classifier()
+        # plt.show()
+
         # Append a rule as a tuple of an array of (feature, value) conditions and outcome
         rules_preds.append((new_rule, pred))
 
@@ -45,15 +58,17 @@ def sc(data):
 
     return rules_preds
 
+
 def sc_multiclass(data):
     # Array of tuples - (class, number of instances of that class)
     y = pd.DataFrame(data.iloc[:, -1])
-    classes_counts = [(c, sum(y.iloc[:, 0] == c)) for c in y.iloc[:, 0].unique()]
+    classes_counts = [(c, sum(y.iloc[:, 0] == c))
+                      for c in y.iloc[:, 0].unique()]
     classes_counts.sort(key=lambda x: x[1])
 
     rules = []
 
-    print(classes_counts, y.value_counts(ascending=True))
+    # print(classes_counts, y.value_counts(ascending=True))
 
     # While there are classes for which the rule hasn't been set yet
     while len(classes_counts) > 1:
@@ -62,25 +77,17 @@ def sc_multiclass(data):
         # Current class for which a rule should be set
         current_class = classes_counts[0][0]
 
-        print(f'Current class: {current_class}')
+        # print(f'Current class: {current_class}')
 
         # Copy of the dataset to be modified to binary classification
-        # yc = y.copy()
-        # yc.loc[y.iloc[:, 0] == current_class] = 1
-        # yc.loc[y.iloc[:, 0] != current_class] = 0
         data_current = data.copy()
-        data_current.loc[data.iloc[:, -1] == current_class, 'Preoperative Diagnosis'] = 1
-        data_current.loc[data.iloc[:, -1] != current_class, 'Preoperative Diagnosis'] = 0
-        print(data_current.loc[data_current['Preoperative Diagnosis'] == 1])
-
-        # if current_class == 3:
-        #     for i in range(len(data_current)):
-        #         if data_current.iloc[i, -1] != 0:
-        #             print(data_current.iloc[i])
+        data_current.loc[data.iloc[:, -1] ==
+                         current_class, 'Preoperative Diagnosis'] = 1
+        data_current.loc[data.iloc[:, -1] !=
+                         current_class, 'Preoperative Diagnosis'] = 0
 
         # Calculating the rule for the current class
         rules_preds_bin = sc(data_current)
-        # rules_binary = sc(X.merge(yc, left_index=True, right_index=True))
 
         # Remove all covered instances using rules_binary
         for r, pred in rules_preds_bin:
@@ -89,31 +96,41 @@ def sc_multiclass(data):
                 data = remove_covered_instances(data, r)
 
         # Add the new rules to result
-        rules.append({'class': current_class, 'rules': rules_preds_bin})
+        rules.append({'class': current_class, 'rules_preds': rules_preds_bin})
 
         # Removing the class for which the rule has been calculated
         classes_counts.remove(classes_counts[0])
 
     # Adding a default rule
-    rules.append({'class': classes_counts[0][0], 'rules': [('default', classes_counts[0][0])]})
+    rules.append({
+        'class': classes_counts[0][0],
+        'rules_preds': [([{
+            'feature': 'default',
+            'operator': '',
+            'threshold': ''
+        }], classes_counts[0][0])]})
 
     return rules
 
-# data = pd.read_csv('../encoded_categorical_binary.csv', index_col=False)
-data = pd.read_csv('../encoded_categorical.csv', index_col=False)
+
+def print_rules_preds(rules_preds):
+    for rule, pred in rules_preds:
+        text = ' AND '.join(
+            [f"{condition['feature']} {condition['operator']} {condition['threshold']}" for condition in rule])
+        print(f"For rule {text} --> Predicted class: {pred}")
+
+
+# data = pd.read_csv('../encoded_binary.csv', index_col=False)
+data = pd.read_csv('../encoded.csv', index_col=False)
 data.reset_index(drop=True, inplace=True)
 
 # rules_preds = sc(data)
-
 # print("Learned Rules:")
-# for rule, pred in rules_preds:
-#     print(f"For rule {rule} --> Predicted class: {pred}")
+# print_rules_preds(rules_preds)
 
 result = sc_multiclass(data)
-
 print("Learned Rules:")
 for res in result:
     print(f"Rules for class {res['class']}:")
-    print(res['rules'])
-    for rule, pred in res['rules']:
-        print(f"For rule {rule} --> Predicted class: {pred}")
+    print_rules_preds(res['rules_preds'])
+    print()
